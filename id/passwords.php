fdd410517df79ed4f8ab6859a8a74c9ea53767ff
Sabar Tampubolon <sabartampubolon@gmail.com>
2020-12-10 10:25:49 +0700

    Create passwords.php

---
+++ id/passwords.php
@@ -0,0 +1,20 @@
+<?php
+
+return [
+    /*
+    |--------------------------------------------------------------------------
+    | Password Reset Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are the default lines which match reasons
+    | that are given by the password broker for a password update attempt
+    | has failed, such as for an invalid token or invalid new password.
+    |
+    */
+
+    'reset'     => 'Kata sandi Anda berhasil diatur ulang.',
+    'sent'      => 'Kami telah mengirimkan tautan ke email Anda!',
+    'throttled' => 'Silahkan tunggu beberapa saat sebelum mencoba kembali.',
+    'token'     => 'Kode yang Anda gunakan tidak terdaftar.',
+    'user'      => 'Alamat email Anda tidak terdaftar.',
+];
